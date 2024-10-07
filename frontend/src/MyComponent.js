87cf406fcc36c80155bdb290b96af073c82ed879
Norbert Koziana <koziana11norbert@gmail.com>
2024-10-07 14:25:01 +0200

    revert changes from frontend

--- frontend/src/MyComponent.js
+++ frontend/src/MyComponent.js
@@ -1,10 +1,11 @@
 
 function MyComponent() {
-  return (
-        <p id="paragraphWithTen">
-            10
-        </p>
-  );
-}
-
-export default MyComponent;
+    return (
+          <p id="paragraphWithTen">
+              10
+          </p>
+    );
+  }
+  
+  export default MyComponent;
+  
